d8d661000930aef36c8742130574b96c34f2c0bd
Josué Lopes <josue.s.lopes.512@gmail.com>
2021-10-21 09:30:40 -0300

    Update app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication
+from dialog import Ui_Dialog
+
+#  pyuic5 .\untitled.ui > dialog.py
+class AppWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = AppWindow()
+    w.show()
+    sys.exit(app.exec_())
