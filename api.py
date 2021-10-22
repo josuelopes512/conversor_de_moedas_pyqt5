e6f1367c165425350f173e23f1611f14429c6d37
Josué Lopes <josue.s.lopes.512@gmail.com>
2021-10-22 09:22:25 -0300

    functions to class

--- api.py
+++ api.py
@@ -1,39 +1,43 @@
 import requests
 import xmltodict
 
-def moeda(de, para):
-    req = requests.get(f"https://economia.awesomeapi.com.br/json/all/{de}-{para}")
-    if(req.status_code == 200):
-        return ((req.json()).get(de, None)).get("high", None)
 
+class ApiConversor:
+    def __init__(self):
+        self.api = "https://economia.awesomeapi.com.br"
+    
+    def moeda(self, moeda_consulta, moeda_local):
+        req = requests.get(f"{self.api}/json/all/{moeda_consulta}-{moeda_local}")
+        if(req.status_code == 200):
+            return ((req.json()).get(moeda_consulta, None)).get("high", None)
+    
+    def conversor(self, moeda_consulta, moeda_local, valor):
+        if moeda_local != moeda_consulta:
+            if type(valor) != float:
+                valor = float(valor)
+                return "{:.2f}".format((valor * float(self.moeda(moeda_consulta, moeda_local))))
+        return "Moeda Inválida"
+    
+    def get_todas_as_moedas(self):
+        req = requests.get(f"{self.api}/xml/available/uniq")
+        if(req.status_code == 200):
+            xparse = xmltodict.parse(req.text)
+            json_ = xparse.get("xml", None)
+            return [i for i, _ in json_.items()]
+    
+    def key_to_value(self, key):
+        req = requests.get(f"{self.api}/xml/available/uniq")
+        if(req.status_code == 200):
+            xparse = xmltodict.parse(req.text)
+            json_ = xparse.get("xml", None)
+            return json_.get(key, None)
 
-def conversor(moeda_input, moeda_consulta, valor):
-    if moeda_input != moeda_consulta:
-        if type(valor) != float:
-            valor = float(valor)
-        return "{:.2f}".format((valor * float(moeda(moeda_consulta, moeda_input))))
-    return "Moeda Inválida"
-
-def todas_as_moedas():
-    req = requests.get("https://economia.awesomeapi.com.br/xml/available/uniq")
-    if(req.status_code == 200):
-        xparse = xmltodict.parse(req.text)
-        json_ = xparse.get("xml", None)
-        return [i for i, _ in json_.items()]
-
-def key_to_value(key):
-    req = requests.get("https://economia.awesomeapi.com.br/xml/available/uniq")
-    if(req.status_code == 200):
-        xparse = xmltodict.parse(req.text)
-        json_ = xparse.get("xml", None)
-        result = json_.get(key, None)
-        return result
-
-de = "USD"
+de = "BRL"
 para = "USD"
 valor = 12
 
-total = conversor(de, para, 12)
-print(f"{valor} {para} = {total} {de}")
-# print(todas_as_moedas())
-# print(key_to_value("BRL"))
+moeda = ApiConversor()
+# total = moeda.conversor(de, para, 12)
+# print(f"{valor} {de} = {total} {para}")
+print(moeda.get_todas_as_moedas())
+print(moeda.key_to_value("BRL"))
