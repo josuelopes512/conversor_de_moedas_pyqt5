5cacbbeb0400ae8ad2de8515b677deae926d52b9
Josué Lopes <josue.s.lopes.512@gmail.com>
2021-10-23 21:21:41 -0300

    new updates

--- api.py
+++ api.py
@@ -1,43 +1,76 @@
 import requests
 import xmltodict
 
-
 class ApiConversor:
     def __init__(self):
         self.api = "https://economia.awesomeapi.com.br"
-    
-    def moeda(self, moeda_consulta, moeda_local):
-        req = requests.get(f"{self.api}/json/all/{moeda_consulta}-{moeda_local}")
+        self.keys = self.get_keys()
+        self.todas_as_moedas = sorted([i for i, _ in self.keys.items()])
+        self.carrega_combinacoes = self.essas_combinacoes()
+        self.moeda_consulta = ""
+        self.moeda_local = ""
+
+    def essas_combinacoes(self):
+        dic = {}
+        req = requests.get(f"{self.api}/json/available")
         if(req.status_code == 200):
-            return ((req.json()).get(moeda_consulta, None)).get("high", None)
-    
+            combinacoes = sorted([i for i, _ in req.json().items()])
+            for j in combinacoes:
+                a = j.split("-")
+                if a[0] not in dic:
+                    dic.update({a[0]:[]})
+                    dic[a[0]].append(a[1])
+                else:
+                    dic[a[0]].append(a[1])
+            return dic
+
+    def is_possui_combinacao(self):
+        result = self.carrega_combinacoes.get(self.moeda_consulta, None)
+        return(True if self.moeda_local in result else None)
+
+    def moeda(self):
+        req = requests.get(f"{self.api}/json/all/{self.moeda_consulta}-{self.moeda_local}")
+        if(req.status_code == 200):
+            # print(req.json())
+            return ((req.json()).get(self.moeda_consulta, None)).get("high", None)
+
     def conversor(self, moeda_consulta, moeda_local, valor):
-        if moeda_local != moeda_consulta:
+        self.moeda_consulta = moeda_consulta
+        self.moeda_local = moeda_local
+
+        if moeda_local != moeda_consulta and self.is_possui_combinacao():
+            moeda = self.moeda()
             if type(valor) != float:
                 valor = float(valor)
-                return "{:.2f}".format((valor * float(self.moeda(moeda_consulta, moeda_local))))
+                return "{:.2f}".format((valor * float(moeda)))
+            else:
+                total = "{:.2f}".format(valor * float(moeda))
+                return float(total)
+        if not self.is_possui_combinacao():
+            return "Moeda Não Encontrada"
         return "Moeda Inválida"
     
-    def get_todas_as_moedas(self):
+    def get_keys(self):
         req = requests.get(f"{self.api}/xml/available/uniq")
         if(req.status_code == 200):
             xparse = xmltodict.parse(req.text)
             json_ = xparse.get("xml", None)
-            return [i for i, _ in json_.items()]
+            return json_
     
     def key_to_value(self, key):
-        req = requests.get(f"{self.api}/xml/available/uniq")
-        if(req.status_code == 200):
-            xparse = xmltodict.parse(req.text)
-            json_ = xparse.get("xml", None)
-            return json_.get(key, None)
+        return self.keys[key]
 
-de = "BRL"
-para = "USD"
-valor = 12
 
-moeda = ApiConversor()
+# de = "BRL"
+# para = "USD"
+# valor = 12
+
+# moeda = ApiConversor()
+# print(moeda.key_to_value("BRL"))
+# print(moeda.is_possui_combinacao())
 # total = moeda.conversor(de, para, 12)
+# print(moeda.carrega_combinacoes)
+# print(type(total), type(valor))
 # print(f"{valor} {de} = {total} {para}")
-print(moeda.get_todas_as_moedas())
-print(moeda.key_to_value("BRL"))
+# print(moeda.get_todas_as_moedas())
+# print(moeda.key_to_value("BRL"))
