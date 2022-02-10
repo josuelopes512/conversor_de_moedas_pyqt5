efda687ba6f408fb6fad013fd159cb468c3fe79d
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-02-10 02:18:07 +0000

    persistent method json file

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import requests
-import xmltodict
+import xmltodict, json
 
 class ApiConversor:
     def __init__(self):
@@ -9,9 +9,12 @@ class ApiConversor:
         self.carrega_combinacoes = self.essas_combinacoes()
         self.moeda_consulta = ""
         self.moeda_local = ""
+        self.all_moedas = None
+        if not self.all_moedas:
+            self.fetchall_moedas()
 
     def essas_combinacoes(self):
-        dic = {}
+        dic = dict()
         req = requests.get(f"{self.api}/json/available")
         if(req.status_code == 200):
             combinacoes = sorted([i for i, _ in req.json().items()])
@@ -22,16 +25,41 @@ class ApiConversor:
                     dic[a[0]].append(a[1])
                 else:
                     dic[a[0]].append(a[1])
-            return dic
+        return dic
 
     def is_possui_combinacao(self):
         result = self.carrega_combinacoes.get(self.moeda_consulta, None)
         return(True if self.moeda_local in result else None)
 
     def moeda(self):
-        req = requests.get(f"{self.api}/json/all/{self.moeda_consulta}-{self.moeda_local}")
+        if self.all_moedas:
+            for moeda in self.all_moedas[self.moeda_consulta]:
+                if moeda['codein'] == self.moeda_local:
+                    return "{:.2f}".format(float(moeda['high']))
+        else:
+            req = requests.get(f"{self.api}/json/all/{self.moeda_consulta}-{self.moeda_local}")
+            if(req.status_code == 200):
+                return "{:.2f}".format(float(((req.json()).get(self.moeda_consulta, None)).get("high", None)))
+        return "{:.2f}".format(float("0.00"))
+    
+    def setMoedas(self, moeda_consulta, moeda_local):
+        req = requests.get(f"{self.api}/json/all/{moeda_consulta}-{moeda_local}")
         if(req.status_code == 200):
-            return "{:.2f}".format(float(((req.json()).get(self.moeda_consulta, None)).get("high", None)))
+            return req.json()
+        else:
+            return dict()
+    
+    def fetchall_moedas(self):
+        print("Carregando Moedas......")
+        try:
+            with open('file.json', 'r') as f:
+                self.all_moedas = json.loads(f.read())
+        except:
+            self.all_moedas = {k: [[(v) for _, v in self.setMoedas(k, moeda_local).items()][-1] for moeda_local in v] for k, v in self.carrega_combinacoes.items()}
+            with open('file.json', 'w') as f:
+                f.write(json.dumps(self.all_moedas))
+            del self.all_moedas
+            self.fetchall_moedas()
 
     def conversor(self, moeda_consulta, moeda_local, valor):
         self.moeda_consulta = moeda_consulta
